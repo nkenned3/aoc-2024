1c4d33831e40399b0531eca9a622f74b8ee0343a
Kennedy, Neal <NeKennedy@geico.com>
2024-12-03 14:50:40 -0800

    save latest in puzzle .go

--- src/advent-of-code-2024/puzzle.go
+++ src/advent-of-code-2024/puzzle.go
@@ -5,5 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Marry Christmas!")
 	fmt.Println("day 2 part 1 solution: ", solveMainDay2())  //534
-	fmt.Println("day 2 part 2 solution: ", solveDay2Part2()) //577
+	fmt.Println("day 2 part 2 solution: ", solveDay2Part2())
 }
